06250b9203407f20b546e724d31c7bb5e3fcfd36
micro0o <micro0o2011@gmail.com>
2022-10-02 13:10:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngx-spinner
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-rtfpe4)
---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  constructor(private spinner: NgxSpinnerService) {}
+
+  ngOnInit() {
+    this.spinner.show();
+    // setTimeout(() => {
+    //   this.spinner.hide();
+    // }, 2000);
+  }
+
+  showSpinner() {
+    this.spinner.show();
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 3000);
+  }
+}
